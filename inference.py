7e7abf18c4037fdd2f813ba57078a763fa0ffd3f
itizazAhmad <itizazahmad06@gmail.com>
2024-02-24 11:58:34 +0500

    update

--- inference.py
+++ inference.py
@@ -19,17 +19,16 @@ if not os.path.exists(Path(config.inference_images)):
     print("please make directory inference_images and put test_image.jpg in that file to do inference")
 else:
     all_images = Path(config.inference_images).glob('*')
-    for image in all_images:
-        image_path = Path(config.inference_images).joinpath(image)
-        image = Image.open(image_path)
+    df = pd.read_csv(Path(config.result_folder_path).joinpath("classes.csv"))
+    for image_path in all_images:
+        image = Image.open(Path(image_path))
         image = image.resize((224, 224))
         image_array = np.array(image)
         image_array = image_array / 255.0 
         image_array = image_array.astype(np.float32)
         input_data = np.expand_dims(image_array, axis=0)
+        input_data = input_data.reshape(1,3,224,224)
         outputs = ort_session.run(None, {'input': input_data})
-
-        df = pd.read_csv(Path(config.result_folder_path).joinpath("classes.csv"))
         output_data = outputs[0]
         index = np.argmax(output_data)
-        print(f"Orignal label: {image} and Predicted lable: {df['0'].iloc[index]}")
+        print(f"Orignal Label: {str(image_path).split('/')[-1].split('.')[-2]} And Predicted Lable: {df['0'].iloc[index]}")
